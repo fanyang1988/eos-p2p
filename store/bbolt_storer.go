9b2273277e60630fe158196db6c494ccaf6393be
Fan Yang <fanyang.coder@gmail.com>
2019-12-24 01:38:18 +0800

    update storer to add api for p2p

--- store/bbolt_storer.go
+++ store/bbolt_storer.go
@@ -1,7 +1,9 @@
 package store
 
 import (
+	"encoding/hex"
 	"encoding/json"
+	"sync"
 	"time"
 
 	"github.com/pkg/errors"
@@ -13,11 +15,12 @@ import (
 
 // BlockDBState head state and chain state
 type BlockDBState struct {
-	HeadBlockNum             uint32            `json:"headNum"`
-	HeadBlockID              types.Checksum256 `json:"headID"`
-	HeadBlockTime            time.Time         `json:"t"`
-	LastIrreversibleBlockNum uint32            `json:"IrrNum"`
-	LastIrreversibleBlockID  types.Checksum256 `json:"IrrID"`
+	HeadBlockNum             uint32             `json:"headNum"`
+	HeadBlockID              types.Checksum256  `json:"headID"`
+	HeadBlockTime            time.Time          `json:"t"`
+	LastIrreversibleBlockNum uint32             `json:"irrNum"`
+	LastIrreversibleBlockID  types.Checksum256  `json:"irrID"`
+	HeadBlock                *types.SignedBlock `json:"blk"`
 }
 
 // NewBlockDBState new stat
@@ -42,28 +45,40 @@ type BBoltStorer struct {
 	chainID types.Checksum256
 	db      *bolt.DB
 	logger  *zap.Logger
+	mutex   sync.RWMutex
 
 	// current store state
 	state *BlockDBState
 }
 
 // NewBBoltStorer create a bbolt storer
-func NewBBoltStorer(logger *zap.Logger, chainID types.Checksum256, dbPath string) (*BBoltStorer, error) {
+func NewBBoltStorer(logger *zap.Logger, chainID string, dbPath string) (*BBoltStorer, error) {
+	cID, err := hex.DecodeString(chainID)
+	if err != nil {
+		return nil, errors.Wrapf(err, "decode chainID error")
+	}
+
 	db, err := bolt.Open(dbPath, 0666, nil)
 	if err != nil {
 		return nil, errors.Wrapf(err, "create storer %s", dbPath)
 	}
 
-	return &BBoltStorer{
-		chainID: chainID,
+	res := &BBoltStorer{
+		chainID: cID,
 		db:      db,
 		logger:  logger,
 		state:   NewBlockDBState(),
-	}, nil
+	}
+
+	if err := res.initState(); err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
 
 func (s *BBoltStorer) initState() error {
-	return s.db.Update(func(tx *bolt.Tx) error {
+	return errors.Wrap(s.db.Update(func(tx *bolt.Tx) error {
 		stateBucket, err := tx.CreateBucketIfNotExists([]byte("state"))
 		if err != nil {
 			return errors.Wrap(err, "initState create")
@@ -90,51 +105,122 @@ func (s *BBoltStorer) initState() error {
 		}
 
 		return nil
-	})
+	}), "init state")
 }
 
 // ChainID get chainID
 func (s *BBoltStorer) ChainID() types.Checksum256 {
+	// const
 	return s.chainID
 }
 
 // HeadBlockNum get headBlockNum
 func (s *BBoltStorer) HeadBlockNum() uint32 {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 	return s.state.HeadBlockNum
+}
+
+// State get state data
+func (s *BBoltStorer) State() BlockDBState {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+	return *s.state
+}
+
+// HeadBlock get head block
+func (s *BBoltStorer) HeadBlock() *types.SignedBlock {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+	if s.state.HeadBlock == nil {
+		return nil
+	}
+	return s.state.HeadBlock // TODO: need deep copy
 }
 
 // HeadBlockID get HeadBlockID
 func (s *BBoltStorer) HeadBlockID() types.Checksum256 {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 	return s.state.HeadBlockID
 }
 
 // LastIrreversibleBlockNum get LastIrreversibleBlockNum
 func (s *BBoltStorer) LastIrreversibleBlockNum() uint32 {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 	return s.state.LastIrreversibleBlockNum
 }
 
 // LastIrreversibleBlockID get LastIrreversibleBlockID
 func (s *BBoltStorer) LastIrreversibleBlockID() types.Checksum256 {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 	return s.state.LastIrreversibleBlockID
 }
 
 func (s *BBoltStorer) updateStatByBlock(blk *types.SignedBlock) error {
+	// Just set to block state
+	s.state.HeadBlockNum = blk.BlockNumber()
+	s.state.HeadBlockID, _ = blk.BlockID()
+	s.state.HeadBlockTime = blk.Timestamp.Time
+	s.state.LastIrreversibleBlockNum = s.state.HeadBlockNum
+	s.state.LastIrreversibleBlockID = s.state.HeadBlockID
+
+	if s.state.HeadBlockNum%1000 == 0 {
+		s.logger.Info("on block head", zap.Uint32("blockNum", s.state.HeadBlockNum))
+	}
+
 	return nil
+}
+
+func (s *BBoltStorer) setBlock(blk *types.SignedBlock) error {
+	return errors.Wrapf(s.db.Update(func(tx *bolt.Tx) error {
+		blockBucket, err := tx.CreateBucketIfNotExists([]byte("state"))
+		if err != nil {
+			return errors.Wrap(err, "create bucket")
+		}
+
+		bID, _ := blk.BlockID()
+
+		jsonBytes, err := json.Marshal(*blk)
+		if err != nil {
+			return errors.Wrap(err, "json")
+		}
+
+		// Use json to shown for test
+		if err := blockBucket.Put([]byte(bID), jsonBytes); err != nil {
+			return errors.Wrap(err, "put block")
+		}
+
+		return nil
+	}), "set block %d", blk.BlockNumber())
 }
 
 // CommitBlock commit block from p2p
 func (s *BBoltStorer) CommitBlock(blk *types.SignedBlock) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 
-	return nil
+	if err := s.updateStatByBlock(blk); err != nil {
+		return err
+	}
+
+	return s.setBlock(blk)
 }
 
 // CommitTrx commit trx
 func (s *BBoltStorer) CommitTrx(trx *types.PackedTransactionMessage) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	return nil
 }
 
 // Flush flush to db
 func (s *BBoltStorer) Flush() error {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
 	tx, err := s.db.Begin(true)
 	if err != nil {
 		return err
--- store/bbolt_storer_test.go
+++ store/bbolt_storer_test.go
@@ -1,21 +1,17 @@
 package store
 
-import "testing"
+import (
+	"testing"
 
-import "go.uber.org/zap"
-
-import "github.com/fanyang1988/eos-p2p/types"
+	"go.uber.org/zap"
+)
 
 func TestStoreInit(t *testing.T) {
 	l, _ := zap.NewDevelopment()
 	defer l.Sync()
-	s, err := NewBBoltStorer(l, types.Checksum256([]byte("")), "./testblocks.db")
+	s, err := NewBBoltStorer(l, "", "./testblocks.db")
 	if err != nil {
 		t.Fatalf("error by new %s", err.Error())
 	}
 	defer s.Close()
-
-	if err := s.initState(); err != nil {
-		t.Fatalf("error by init %s", err.Error())
-	}
 }
